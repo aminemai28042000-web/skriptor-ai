f99e3c94c15527c93067c5a2b9620d4186bacb7b
aminemai28042000-web <aminemai28042000@gmail.com>
2025-12-01 23:51:23 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,35 @@
+import asyncio
 from fastapi import FastAPI, Request
 from aiogram import Bot
+import aiohttp
 from worker_queue import enqueue_task
 from config import TELEGRAM_TOKEN, WEBHOOK_SECRET, WEBHOOK_URL
-import hashlib
 
 bot = Bot(token=TELEGRAM_TOKEN)
-
 app = FastAPI()
 
 
 def verify_secret(request: Request):
     header = request.headers.get("X-Telegram-Bot-Api-Secret-Token")
     return header == WEBHOOK_SECRET
+
+
+@app.on_event("startup")
+async def on_startup():
+    """
+    Устанавливаем вебхук при запуске контейнера.
+    """
+    async with aiohttp.ClientSession() as session:
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/setWebhook"
+
+        payload = {
+            "url": WEBHOOK_URL,
+            "secret_token": WEBHOOK_SECRET
+        }
+
+        async with session.post(url, data=payload) as resp:
+            data = await resp.text()
+            print("WEBHOOK SET RESPONSE:", data)
 
 
 @app.post("/webhook")
@@ -21,10 +39,9 @@ async def webhook(request: Request):
 
     update = await request.json()
 
-    # только ссылки на скачивание
     if "message" in update and "text" in update["message"]:
-        url = update["message"]["text"]
         chat_id = update["message"]["chat"]["id"]
+        url = update["message"]["text"]
 
         await enqueue_task({"chat_id": chat_id, "url": url})
 
